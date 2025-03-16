36d1af2417f70c755fc2d77994d26b8a5c3a9d91
mkeloo <mkeloo@ufl.edu>
2025-03-16 03:31:13 -0400

    Add flower buds and vapes deals data to the index file

--- lib/index.ts
+++ lib/index.ts
@@ -33,3 +33,189 @@ export const hempCOA = [
     { id: 28, file_name: "White_Truffle_THCA_Flower_COA_09:24.pdf", file_type: "application/pdf", file_size_kb: 308.43, file_url: "White_Truffle_THCA_Flower_COA_09%3A24.pdf" },
     { id: 29, file_name: "White_Widow_THCA_Snowcaps-COA_09:24.pdf", file_type: "application/pdf", file_size_kb: 309.04, file_url: "White_Widow_THCA_Snowcaps-COA_09%3A24.pdf" }
 ];
+
+
+
+
+
+const media_bucket_url = "https://dnltndrwudjaskjwczvm.supabase.co/storage/v1/object/public/inhale-bay-website";
+
+export const flowerBudsDeals = [
+    { id: 1, budName: "GROOVY GRAPE", OneGramPrice: 7.00, FourGramPrice: 24.00, imageSrc: `${media_bucket_url}/flower-buds-deals/3-OG-FlowerBud.webp`, bgGradient: "from-blue-500 via-indigo-500 to-purple-500" },
+    { id: 2, budName: "PINNACLE", OneGramPrice: 7.00, FourGramPrice: 24.00, imageSrc: `${media_bucket_url}/flower-buds-deals/4-OG-FlowerBud.webp`, bgGradient: "from-teal-400 via-cyan-500 to-blue-500" },
+    { id: 3, budName: "LEMON CHERRY GELATO", OneGramPrice: 7.00, FourGramPrice: 24.00, imageSrc: `${media_bucket_url}/flower-buds-deals/6-OG-FlowerBud.webp`, bgGradient: "from-yellow-400 via-green-500 to-teal-500" },
+    { id: 4, budName: "GELATO PUNCH", OneGramPrice: 7.00, FourGramPrice: 24.00, imageSrc: `${media_bucket_url}/flower-buds-deals/7-OG-FlowerBud.webp`, bgGradient: "from-pink-500 via-purple-500 to-indigo-500" },
+    { id: 5, budName: "ICE CREAM CAKE", OneGramPrice: 7.00, FourGramPrice: 24.00, imageSrc: `${media_bucket_url}/flower-buds-deals/10-OG-FlowerBud.webp`, bgGradient: "from-pink-400 via-red-500 to-purple-500" },
+    { id: 6, budName: "GMO GRAPE", OneGramPrice: 7.00, FourGramPrice: 24.00, imageSrc: `${media_bucket_url}/flower-buds-deals/11-OG-FlowerBud.webp`, bgGradient: "from-blue-400 via-purple-500 to-pink-500" },
+    { id: 7, budName: "OG CHEM", OneGramPrice: 7.00, FourGramPrice: 24.00, imageSrc: `${media_bucket_url}/flower-buds-deals/12-OG-FlowerBud.webp`, bgGradient: "from-green-500 via-lime-500 to-teal-400" },
+    { id: 8, budName: "RS 11", OneGramPrice: 7.00, FourGramPrice: 24.00, imageSrc: `${media_bucket_url}/flower-buds-deals/13-OG-FlowerBud.webp`, bgGradient: "from-cyan-400 via-blue-500 to-indigo-500" },
+    { id: 9, budName: "CAKE & BAKE", OneGramPrice: 7.00, FourGramPrice: 24.00, imageSrc: `${media_bucket_url}/flower-buds-deals/31-OG-FlowerBud.webp`, bgGradient: "from-pink-500 via-yellow-500 to-orange-400" },
+    { id: 10, budName: "**WEDDING CRASHER - INDICA", OneGramPrice: 7.00, FourGramPrice: 24.00, imageSrc: `${media_bucket_url}/flower-buds-deals/31-OG-FlowerBud.webp`, bgGradient: "from-pink-500 via-yellow-500 to-orange-400" },
+    { id: 11, budName: "TROPICAL CHERRY", OneGramPrice: 8.00, FourGramPrice: 28.00, imageSrc: `${media_bucket_url}/flower-buds-deals/16-OG-FlowerBud.webp`, bgGradient: "from-teal-500 via-blue-500 to-indigo-500" },
+    { id: 12, budName: "PANDEMIC PUNCH", OneGramPrice: 8.00, FourGramPrice: 28.00, imageSrc: `${media_bucket_url}/flower-buds-deals/19-OG-FlowerBud.webp`, bgGradient: "from-indigo-500 via-purple-400 to-pink-400" },
+    { id: 13, budName: "MOONBOW", OneGramPrice: 8.00, FourGramPrice: 28.00, imageSrc: `${media_bucket_url}/flower-buds-deals/20-OG-FlowerBud.webp`, bgGradient: "from-blue-500 via-cyan-400 to-teal-500" },
+    { id: 14, budName: "GUSHERS", OneGramPrice: 8.00, FourGramPrice: 28.00, imageSrc: `${media_bucket_url}/flower-buds-deals/21-OG-FlowerBud.webp`, bgGradient: "from-yellow-400 via-orange-400 to-red-400" },
+    { id: 15, budName: "GELATO 45", OneGramPrice: 8.00, FourGramPrice: 28.00, imageSrc: `${media_bucket_url}/flower-buds-deals/22-OG-FlowerBud.webp`, bgGradient: "from-cyan-400 via-green-400 to-teal-400" },
+    { id: 16, budName: "GUSH MINTZ", OneGramPrice: 8.00, FourGramPrice: 28.00, imageSrc: `${media_bucket_url}/flower-buds-deals/24-OG-FlowerBud.webp`, bgGradient: "from-green-400 via-teal-400 to-cyan-400" },
+    { id: 17, budName: "GLITTER BOMB", OneGramPrice: 8.00, FourGramPrice: 28.00, imageSrc: `${media_bucket_url}/flower-buds-deals/26-OG-FlowerBud.webp`, bgGradient: "from-green-400 via-yellow-500 to-orange-500" },
+    { id: 18, budName: "**SUNDAY RUNTZ - SATIVA** ", OneGramPrice: 8.00, FourGramPrice: 28.00, imageSrc: `${media_bucket_url}/flower-buds-deals/28-OG-FlowerBud.webp`, bgGradient: "from-green-500 via-teal-500 to-blue-500" },
+    { id: 19, budName: "**KUSH MINT - HYBRID**", OneGramPrice: 8.00, FourGramPrice: 28.00, imageSrc: `${media_bucket_url}/flower-buds-deals/28-OG-FlowerBud.webp`, bgGradient: "from-green-500 via-teal-500 to-blue-500" },
+    { id: 20, budName: "MEAN MAMA", OneGramPrice: 10.00, FourGramPrice: 35.00, imageSrc: `${media_bucket_url}/flower-buds-deals/29-OG-FlowerBud.webp`, bgGradient: "from-red-400 via-orange-400 to-yellow-400" },
+    { id: 21, budName: "HYBRID HEADBAND", OneGramPrice: 10.00, FourGramPrice: 35.00, imageSrc: `${media_bucket_url}/flower-buds-deals/30-OG-FlowerBud.webp`, bgGradient: "from-blue-500 via-cyan-500 to-teal-500" },
+];
+
+
+
+
+export const vapesDeals = [
+    {
+        id: 1,
+        vape_company: "V Touch",
+        base_deal: [
+            {
+                buy_1: "$25",
+                buy_2: "$40"
+            }
+        ],
+        discount_percent: "20",
+        deal_tagline: "BUY 1 for $25, GET 2 for $40",
+        short_title: "V Touch Mega Deal",
+        imgSrc: `${media_bucket_url}/vapes-deals/1-vtouch.webp`,
+        bgGradient: "from-blue-400 to-indigo-900",
+    },
+    {
+        id: 2,
+        vape_company: "South Connect",
+        base_deal: [
+            {
+                buy_1: "$25",
+                buy_2: "$40"
+            }
+        ],
+        discount_percent: "20",
+        deal_tagline: "BUY 1 for $25, GET 2 for $40",
+        short_title: "South Connect Offer",
+        imgSrc: `${media_bucket_url}/vapes-deals/2-south-connect.webp`,
+        bgGradient: "from-purple-400 to-lime-500",
+    },
+    {
+        id: 3,
+        vape_company: "Geek Bar 15K",
+        base_deal: [
+            {
+                buy_1: "$18",
+                buy_2: "$30"
+            }
+        ],
+        discount_percent: "17",
+        deal_tagline: "BUY 1 for $18, GET 2 for $30",
+        short_title: "Geek Bar 15K Promotion",
+        imgSrc: `${media_bucket_url}/vapes-deals/3-geekbar-15k.webp`,
+        bgGradient: "from-yellow-400 to-orange-500",
+    },
+    {
+        id: 4,
+        vape_company: "Lost Mary 20K",
+        base_deal: [
+            {
+                buy_1: "$19",
+                buy_2: "$30"
+            }
+        ],
+        discount_percent: "21",
+        deal_tagline: "BUY 1 for $19, GET 2 for $30",
+        short_title: "Lost Mary Value Pack",
+        imgSrc: `${media_bucket_url}/vapes-deals/4-lost-mary-20k.webp`,
+        bgGradient: "from-orange-400 to-yellow-500",
+    },
+    {
+        id: 5,
+        vape_company: "Geek Bar 25K",
+        base_deal: [
+            {
+                buy_1: "$20",
+                buy_2: "$35"
+            }
+        ],
+        discount_percent: "12.5",
+        deal_tagline: "BUY 1 for $20, GET 2 for $35",
+        short_title: "Geek Bar 25K Deal",
+        imgSrc: `${media_bucket_url}/vapes-deals/5-geekbar-25k.webp`,
+        bgGradient: "from-purple-400 to-pink-500",
+    },
+    {
+        id: 6,
+        vape_company: "Quasar 25K",
+        base_deal: [
+            {
+                buy_1: "$20",
+                buy_2: "$35"
+            }
+        ],
+        discount_percent: "12.5",
+        deal_tagline: "BUY 1 for $20, GET 2 for $35",
+        short_title: "Quasar 25K Savings",
+        imgSrc: `${media_bucket_url}/vapes-deals/6-quasar-25k.webp`,
+        bgGradient: "from-blue-400 to-lime-500",
+    },
+    {
+        id: 7,
+        vape_company: "Fume Extra",
+        base_deal: [
+            {
+                buy_1: "$10",
+                buy_2: "$17"
+            }
+        ],
+        discount_percent: "15",
+        deal_tagline: "BUY 1 for $10, GET 2 for $17",
+        short_title: "Fume Extra Limited Deal",
+        imgSrc: `${media_bucket_url}/vapes-deals/7-fume-extra.webp`,
+        bgGradient: "from-blue-400 to-indigo-900",
+    },
+    {
+        id: 8,
+        vape_company: "Off Stamp 9K No Kit",
+        base_deal: [
+            {
+                buy_1: "$10",
+                buy_2: "$17"
+            }
+        ],
+        discount_percent: "15",
+        deal_tagline: "BUY 1 for $10, GET 2 for $17",
+        short_title: "Off Stamp 9K No Kit Offer",
+        imgSrc: `${media_bucket_url}/vapes-deals/8-off-stamp-no-kit.webp`,
+        bgGradient: "from-green-400 to-orange-500",
+    },
+    {
+        id: 9,
+        vape_company: "Off Stamp 9K with Kit",
+        base_deal: [
+            {
+                buy_1: "$16",
+                buy_2: "$25"
+            }
+        ],
+        discount_percent: "22",
+        deal_tagline: "BUY 1 for $16, GET 2 for $25",
+        short_title: "Off Stamp 9K Kit Discount",
+        imgSrc: `${media_bucket_url}/vapes-deals/9-off-stamp-kit.webp`,
+        bgGradient: "from-green-400 to-lime-500",
+    },
+    {
+        id: 10,
+        vape_company: "Vape Watch",
+        base_deal: [
+            {
+                buy_1: "$25",
+                buy_2: "$40"
+            }
+        ],
+        discount_percent: "20",
+        deal_tagline: "BUY 1 for $25, GET 2 for $40",
+        short_title: "Vape Watch Combo Deal",
+        imgSrc: `${media_bucket_url}/vapes-deals/10-vape-watch.webp`,
+        bgGradient: "from-orange-400 to-yellow-500",
+    }
+];
